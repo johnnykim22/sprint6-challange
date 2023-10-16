594a914b36901fe894ab52182df2a2bc4d54a680
johnnykim22 <johnnykim22@gmail.com>
2023-10-16 16:26:15 -0700

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -8,15 +8,32 @@ const urlPeople = 'http://localhost:9009/api/people'
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+  const [peopleData, setPeopleData] = useState([])
+  const [planetData, setPlanetData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:9009/api/people')
+    .then(res => setPeopleData(res.data))
+    .catch(err => console.log(err))
+
+    axios.get('http://localhost:9009/api/planets')
+    .then(res => setPlanetData(res.data))
+    .catch(err => console.log(err))
+  },  [])
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {peopleData.map((person, index) => {
+  const test = planetData.find(planet => planet.id === person.homeworld)
+  return (<Character key={index} character={person} planet={test} />);
+})}
     </div>
   )
 }
-
+ 
 export default App
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character(props) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+ const [showHomeworld, setShowHomeworld] = useState(false);
+
+ const toggleHomeworld = () => {
+    setShowHomeworld(!showHomeworld);
+  };
+
+  // const togglePlanet = () => {  
+  //   if (props.character.name && props.character.name[planetID]) {
+  //     const planetName = props.character.name[planetID].name;
+  //   }
+  // };
+ 
+ 
+ 
+ 
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className="character-card" onClick={toggleHomeworld}>
+      <h3 className="character-name">{props.character.name}</h3>
+      
+     
+    
+
+     
+      
+      {showHomeworld && (
+        <div className="homeworld-info">
+           {/* <p onClick={togglePlanet}> */}
+        Planet: <span className="character-planet">{props.planet.name}</span>
+      {/* </p> */}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Character
+export default Character;
